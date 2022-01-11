c8b4590e7abfcb0aa060de801b1fbf64ffce7e44
Hussain Iftikhar <95127768+hussainiftikhar5242@users.noreply.github.com>
2022-01-11 19:19:24 +0500

    this will display x value from 1 to 10 using loop

---
+++ task1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+void counting();
+main()
+{
+  counting();  
+}
+void counting()
+{
+    for(int x = 1; x<=10 ; x++)
+    {
+        cout<<x<<endl;
+    }
+}
