e3460d9ccc8bdfc59a64873dd3af95327771909c
Hussain Iftikhar <95127768+hussainiftikhar5242@users.noreply.github.com>
2022-01-11 19:33:24 +0500

    this program will diaplay first ten number using while loop

---
+++ task9.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+void counting();
+main()
+{
+  counting();  
+}
+void counting()
+{
+    int x = 1;
+    while( x<=10 )
+    {
+        cout<<x<<endl;
+        x++;
+    }
+}
